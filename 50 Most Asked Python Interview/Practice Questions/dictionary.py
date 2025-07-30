79774b32bff7977ed24818fa72acee3eb6bb2d87
Kartik Sharma <ksb40968@gmail.com>
2025-07-30 11:07:11 +0530

    python questions

---
+++ 50 Most Asked Python Interview/Practice Questions/dictionary.py
@@ -0,0 +1,13 @@
+# Dictionary
+
+# Stores key-value pair
+# dict. are immutable
+
+student = {'name': 'Kartik',
+           'age' : 21,
+           'branch' : 'CSE'
+           }
+print(student)
+
+student['age'] = 23
+print(student)
