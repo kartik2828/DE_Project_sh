8c12c97e87e562186a3616b9b27384defeee5d19
Kartik Sharma <ksb40968@gmail.com>
2025-08-08 11:02:06 +0530

    add abstraction

--- Python/Oops/Abstraction/demo.py
+++ Python/Oops/Abstraction/demo.py
@@ -1,3 +1,41 @@
 '''
 Abstraction means hiding complex implementation details and showing only the necessary features to the user.
 '''
+
+'''
+Achieved in Python mainly using:
+
+Abstract Classes (from abc module)
+
+Interfaces (Python doesn’t have direct interfaces, but abstract classes can act as them)
+'''
+
+from abc import ABC, abstractmethod
+
+class Vehicle(ABC):
+
+    @abstractmethod
+    def start(self):
+        pass
+
+    @abstractmethod
+    def stop(self):
+        pass
+
+class Car(Vehicle):
+    def start(self):
+        print("Car Started")
+    def stop(self):
+        print("Car stopped")    
+
+class Bike(Vehicle):
+    def start(self):
+        print("Bike started")
+    def stop(self):
+        print("Bike stop")    
+
+list = [Car(),Bike()]
+
+for i in list:
+    i.start()
+    i.stop()
